c889d542293b2461e03f61b711f1716ac5986459
Jack Schneble <schneble.jack@gmail.com>
2022-10-19 22:34:16 -0400

    I wanted to showcase this as a generic weather application so I removed the title name

--- pages/index.js
+++ pages/index.js
@@ -55,33 +55,33 @@ export default function Home() {
         <title>Weather Forecast App</title>
       
       </Head>
-      <div className='absolute  top-0 left-0 right-0 bottom-0  backdrop-brightness-50 bg-black/30 ' />
-     <div className='bg-nature-light dark:bg-nature-dark w-full h-screen object-cover bg-cover bg-center  ' >
+      <div className='absolute top-0 bottom-0 left-0 right-0 backdrop-brightness-50 bg-black/30 ' />
+     <div className='object-cover w-full h-screen bg-center bg-cover bg-nature-light dark:bg-nature-dark ' >
     
      <button
           onClick={switchTheme}
-          className="absolute top-5 right-9 h-13 w-13  bg-size-60  bg-white-100  dark:text-yellow rounded-full capitalize">
+          className="absolute capitalize rounded-full top-5 right-9 h-13 w-13 bg-size-60 bg-white-100 dark:text-yellow">
           
-          <BsCloudSun className='text-skyblue dark:text-yellow dark:border-color-skyblue bg-white rounded-lg border-2 text-5xl' />
+          <BsCloudSun className='text-5xl bg-white border-2 rounded-lg text-skyblue dark:text-yellow dark:border-color-skyblue' />
       </button>
 
       {/* Search */}
-      <div className='flex absolute ml-2 h-25  rounded-2xl p-8   mt-4 text-left '><div className='pb-2 mb-1 ml-2 '  ><Image  src="/30.png" alt="sun logo" height="60" width="60" /> </div><h1 className=' ml-1 mt-2 tracking-widest  text-4xl text-litewite drop-shadow-2xl font-thin '>Jacks Forecast</h1></div>    
+      <div className='absolute flex p-8 mt-4 ml-2 text-left h-25 rounded-2xl '><div className='pb-2 mb-1 ml-2 '  ><Image  src="/30.png" alt="sun logo" height="60" width="60" /> </div><h1 className='mt-2 ml-1 text-4xl font-thin tracking-widest  text-litewite drop-shadow-2xl'>Forecast</h1></div>    
         <div className='relative flex justify-between items-center max-w-[500px]  w-full m-auto pt-4 px-4 text-black z-10'>
       </div>
  
-      <ul className="  pr-10  text-litewite space-y-3 text-center w-1/2 m-auto absolute top-40 left-80 ml-14 pl-24 mt-30  justify-between text-lg font-extralight  ">
+      <ul className="absolute justify-between w-1/2 pl-24 pr-10 m-auto space-y-3 text-lg text-center  text-litewite top-40 left-80 ml-14 mt-30 font-extralight">
       
-      <li className='  shadow-md shadow-black tracking-wide backdrop-blur-xl bg-skyblue/10 backdrop-brightness-50     pr-10   pt-2 pb-2 pl-6 ml-8 '> Access current weather data for any location including over 200,000 cities </li>
+      <li className='pt-2 pb-2 pl-6 pr-10 ml-8 tracking-wide shadow-md  shadow-black backdrop-blur-xl bg-skyblue/10 backdrop-brightness-50'> Access current weather data for any location including over 200,000 cities </li>
   
-      <li className='    pr-10 shadow-md tracking-wide shadow-black  bg-skyblue/10    backdrop-brightness-50 backdrop-blur-xl  pl-6 p-2 pt-3 ml-8 '>We collect and process weather data from different sources such as global and local weather models, satellites, radars and a vast network of weather stations</li>
+      <li className='p-2 pt-3 pl-6 pr-10 ml-8 tracking-wide shadow-md  shadow-black bg-skyblue/10 backdrop-brightness-50 backdrop-blur-xl'>We collect and process weather data from different sources such as global and local weather models, satellites, radars and a vast network of weather stations</li>
       
        <form 
         onSubmit={fetchWeather}
-        className='flex  ml-4  drop-shadow-lg justify-between items-center w-full m-auto mt-8 backdrop-blur-md shadow-black3 p-3 bg-gradient-to-r from-sarah/20 to-blue-500/20  border  border-maybe font-extralight tracking-wider text-white rounded-2xl'>
+        className='flex items-center justify-between w-full p-3 m-auto mt-8 ml-4 tracking-wider text-white border drop-shadow-lg backdrop-blur-md shadow-black3 bg-gradient-to-r from-sarah/20 to-blue-500/20 border-maybe font-extralight rounded-2xl'>
        <input 
        onChange={(e) => setCity(e.target.value)}
-       className='bg-transparent tracking-wide border-none placeholder:text-litewite text-white focus:outline-none text-2xl ' 
+       className='text-2xl tracking-wide text-white bg-transparent border-none placeholder:text-litewite focus:outline-none ' 
        type="text" placeholder='Search city'  />
           <button  onClick={fetchWeather}>
             <BsSearch/>
