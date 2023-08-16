8bf2bd971fa6dd6c19726adaa214f53c5aa78780
Jack Schneble <schneble.jack@gmail.com>
2023-08-16 02:15:18 -0400

    needed a cleanup, and mobile work

--- components/Forecast.js
+++ components/Forecast.js
@@ -4,37 +4,47 @@ import Image from 'next/image';
 const Forecast = ({data}) => {
   return (
     <div>
-        <div>
-            <div className='absolute flex mt-20 ml-36 bottom-10' >
-            <Image
-             src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
-            alt='/'
-            width='120'
-            height='120'
-          />
-          
+      <div className='text-left rounded bg-gradient-to-r from-red-500 to-pink-500 items-left justify-evenly'>
+        <div className='p-2 z-2'>
+          <div className='flex backdrop-blur-xl' >        
+            <h1 className=' text-7xl'>{data.main.temp.toFixed(0)}&#176;</h1>
+              <Image
+                src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+                alt='/'
+                width='75'
+                height='60'
+              />
           </div>
-          </div>
-          <h1 className='flex pt-4 mt-2 ml-6 text-5xl font-semibold text-again/80'>{data.name}</h1>
-            <h1 className='mt-2 ml-4 text-6xl '>{data.weather[0].main}</h1>
-      
-        <h1 className='flex pl-40 ml-40 text-7xl'>{data.main.temp.toFixed(0)}&#176;</h1>
-     
-      
-      {/* Bottom */}
-
-
-      
-       <div className='flex mt-6 space-x-4 ml-36'>
-        <h1 className='text-xl'>Feels like</h1>
-            <h1 className='text-2xl font-bold'>{data.main.feels_like.toFixed(0)}&#176;</h1>
-           
-        <h1 className='text-xl'>Humidity</h1>
-            <h1 className='text-2xl font-bold'>{data.main.humidity}%</h1>
-           
-        <h1 className='text-xl'>Winds</h1>
-            <h1 className='text-2xl font-bold'>{data.wind.speed.toFixed(0)} MPH</h1>
+      <div className='flex flex-col w-full '>
+        <div className='flex backdrop-blur-xl '>
+          <h1 className='text-xl font-medium tracking-wide text-zinc-900'>
+          Feels like 
+            <span> 
+              {data.main.feels_like.toFixed(0)}&#176;
+            </span>
+         </h1>
         </div>
+        
+          <div className='flex backdrop-blur-xl'>
+           <h1 className='text-xl font-medium tracking-wide text-zinc-900'>
+           Humidity 
+            <span>
+              {data.main.humidity}%
+            </span>
+            </h1>
+          </div>
+       
+          <div className='flex backdrop-blur-xl'>
+            <h1 className='text-xl font-medium tracking-wide text-zinc-900'>
+           Wind 
+              <span>
+              {data.wind.speed.toFixed(0)} mph  
+              </span>
+            </h1>
+          </div>     
+        </div>
+      </div>
+    </div>
  </div>
 
 
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,12 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import axios from 'axios';
 import { useState } from 'react';
 import { BsSearch } from 'react-icons/bs';
-import Post from '../components/Post'
-import { FiSun, FiMoon } from 'react-icons/fi';
-import { FaRegSun } from 'react-icons/fa';
-import { BsCloudSun } from 'react-icons/bs';
 import Spin from '../components/Spin.jsx';
 import Forecast from '../components/Forecast'
 
 export default function Home() {
 
-
-  
-  const LIGHT_THEME = 'light'
-  const DARK_THEME = 'dark'
-  
-
-
-  const [theme, setTheme] = useState(LIGHT_THEME)
-  const switchTheme = () => {
-    if (!document.documentElement.classList.contains(DARK_THEME)) {
-      document.documentElement.classList.add(DARK_THEME)
-      setTheme(DARK_THEME)
-    } else {
-      document.documentElement.classList.remove(DARK_THEME)
-      setTheme(LIGHT_THEME)
-    }
-  }
 
   const [city, setCity] = useState('');
   const [forecast, setForecast] = useState({});
@@ -49,53 +27,43 @@ export default function Home() {
   if (loading) {
     return <Spin />;
   } else {
+
     return (
-    <div>
+    <div >
       <Head>
         <title>Weather Forecast App</title>
-      
       </Head>
-      <div className='absolute top-0 bottom-0 left-0 right-0 backdrop-brightness-50 bg-black/30 ' />
-     <div className='object-cover w-full h-screen bg-center bg-cover bg-nature-light dark:bg-nature-dark ' >
-    
-     <button
-          onClick={switchTheme}
-          className="absolute capitalize rounded-full top-5 right-9 h-13 w-13 bg-size-60 bg-white-100 dark:text-yellow">
-          
-          <BsCloudSun className='text-5xl bg-white border-2 rounded-lg text-skyblue dark:text-yellow dark:border-color-skyblue' />
-      </button>
-
-      {/* Search */}
-      <div className='absolute flex p-8 mt-4 ml-2 text-left h-25 rounded-2xl '><div className='pb-2 mb-1 ml-2 '  ><Image  src="/30.png" alt="sun logo" height="60" width="60" /> </div><h1 className='mt-2 ml-1 text-4xl font-thin tracking-widest  text-litewite drop-shadow-2xl'>Forecast</h1></div>    
-        <div className='relative flex justify-between items-center max-w-[500px]  w-full m-auto pt-4 px-4 text-black z-10'>
-      </div>
- 
-      <ul className="absolute justify-between w-1/2 pl-24 pr-10 m-auto space-y-3 text-lg text-center  text-litewite top-40 left-80 ml-14 mt-30 font-extralight">
-      
-      <li className='pt-2 pb-2 pl-6 pr-10 ml-8 tracking-wide shadow-md  shadow-black backdrop-blur-xl bg-skyblue/10 backdrop-brightness-50'> Access current weather data for any location including over 200,000 cities </li>
-  
-      <li className='p-2 pt-3 pl-6 pr-10 ml-8 tracking-wide shadow-md  shadow-black bg-skyblue/10 backdrop-brightness-50 backdrop-blur-xl'>We collect and process weather data from different sources such as global and local weather models, satellites, radars and a vast network of weather stations</li>
-      
-       <form 
+      <section className='flex items-center justify-center mt-10'>
+        <div className="mx-auto  max-w-md px-2.5 text-center sm:max-w-lg sm:px-0">
+          <h1 className="mt-5 font-display text-4xl font-extrabold leading-[1.15] text-black sm:text-6xl sm:leading-[1.15]">
+            Weather Data
+          <br />
+            <span className="text-transparent bg-gradient-to-r from-red-500 to-pink-500 bg-clip-text">
+            FOR YOU
+            </span>
+          </h1>
+          <h2 className="mt-5 text-gray-600 sm:text-xl">
+          Access current weather data for any location including over 200,000 cities
+          </h2>
+      <div className="flex mx-auto space-x-4 max-w-fit">
+      <form 
         onSubmit={fetchWeather}
-        className='flex items-center justify-between w-full p-3 m-auto mt-8 ml-4 tracking-wider text-white border drop-shadow-lg backdrop-blur-md shadow-black3 bg-gradient-to-r from-sarah/20 to-blue-500/20 border-maybe font-extralight rounded-2xl'>
+        className='flex items-center justify-between w-full p-3 m-auto mt-8 ml-4 text-white border rounded-md bg-zinc-900'>
        <input 
        onChange={(e) => setCity(e.target.value)}
-       className='text-2xl tracking-wide text-white bg-transparent border-none placeholder:text-litewite focus:outline-none ' 
+       className='text-xl font-light bg-transparent border-none placeholder:text-white focus:outline-none ' 
        type="text" placeholder='Search city'  />
           <button  onClick={fetchWeather}>
             <BsSearch/>
           </button>
-        </form>
-      
-   
-    {forecast.main && <Forecast data={forecast} />}
-    </ul>
+        </form> 
+      </div>
+      <div className='flex flex-col items-center justify-center mt-6'>
+        {forecast.main && <Forecast data={forecast} />}
+      </div>
     </div>
-    
-    </div>
-  
-
+  </section>
+ </div>
   );
 };
 };
